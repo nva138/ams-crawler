9cf9d925e14587748ae60ad8ef4395bdc4c93594
Maximilian Mayer <office@maximilianmayer.at>
2026-06-20 13:56:34 +0200

    feat(scraper): scrape all pages and output jobs as JSON

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -1,57 +1,84 @@
 import { chromium } from "playwright";
 
 let nextPage = true;
+const jobsList = [];
 
 (async() => {
-    const browser = await chromium.launch( {headless: false} );
+    const browser = await chromium.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto('https://jobs.ams.at/');
-    await page
-        .getByTestId('job-autocomplete')
-        .getByTestId('ams-autocomplete-input')
-        .fill('FullStack');
-    await page
-        .getByTestId('location-autocomplete')
-        .getByTestId('ams-autocomplete-input')
-        .fill('Wien');
-    await page
-        .getByRole('button', {name: 'Suchen'})
-        .press('Enter');
-    while(nextPage) {
-        await page.waitForLoadState('networkidle');
-        const jobs = await page
-            .getByRole('navigation', {name: 'Suchergebnisse'})
-            .getByRole('listitem')
-            .all();
+    try {
 
-        for (const job of jobs) {
-            const title = await job
-                .getByRole('link')
-                .first()
-                .textContent();
-            const cleanTitle = title?.trim();
-            const company = await job
-                .locator('ams-key-value-pair')
-                .filter({ hasText: 'Unternehmen' })
-                .getByTestId('key-value-pair-content')
-                .textContent();
-            const url = await job
-                .getByRole('link')
-                .first()
-                .getAttribute('href');
-            console.log(company, cleanTitle, url)
-        }
-        const nextButton = page?.getByTestId('ams-pagination-list-next-link');
-        const buttonExcists = await nextButton?.isVisible();
+        await page.goto('https://jobs.ams.at/');
+        await page
+            .getByTestId('job-autocomplete')
+            .getByTestId('ams-autocomplete-input')
+            .fill('FullStack');
+        await page
+            .getByTestId('location-autocomplete')
+            .getByTestId('ams-autocomplete-input')
+            .fill('Wien');
+        await page
+            .getByRole('button', {name: 'Suchen'})
+            .press('Enter');
+        while (nextPage) {
+            await page.waitForLoadState('networkidle');
+            const jobs = await page
+                .getByRole('navigation', {name: 'Suchergebnisse'})
+                .getByRole('listitem')
+                .all();
 
-        if(buttonExcists) {
-            await page?.getByTestId('ams-pagination-list-next-link').click();
-            // await page.waitForTimeout(2000);
+            for (const job of jobs) {
+                const title = await job
+                    .getByRole('link')
+                    .first()
+                    .textContent();
+                const cleanTitle = title?.trim();
+                const company = await job
+                    .locator('ams-key-value-pair')
+                    .filter({hasText: 'Unternehmen'})
+                    .getByTestId('key-value-pair-content')
+                    .textContent();
+                const url = await job
+                    .getByRole('link')
+                    .first()
+                    .getAttribute('href');
+                jobsList.push({
+                    title: cleanTitle,
+                    company: company,
+                    location: "Wien",
+                    description: null,
+                    url: url
+                })
+            }
 
+            const nextButton = page?.getByTestId('ams-pagination-list-next-link');
+            const buttonIsVisible = await nextButton?.isVisible();
+            const isDisabled =  await page.
+            locator('.disabled[data-testid="ams-pagination-list-next"]')
+                .isVisible()
+            console.error("visible:", buttonIsVisible, "disabled:", isDisabled);
+
+            if (buttonIsVisible && !isDisabled) {
+                await page?.getByTestId('ams-pagination-list-next-link').click();
+                await page.waitForTimeout(2000);
+
+            }
+            else {
+                nextPage = false;
+            }
         }
-        else {
-            nextPage = false;
-        }
+        const scraperOutput = JSON.stringify(jobsList)
+        console.log(scraperOutput)
+
     }
-        await page.pause();
+    catch(e){
+        console.error("Scraper failed:", e);
+        throw e
+    }
+    finally {
+
+        await browser.close();
+}
+
 })()
+
